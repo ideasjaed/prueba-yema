c463351b1f6080b4d53abaed2a1c3af899c6935b
unknown <ideas.jaed@gmail.com>
2020-07-10 22:58:08 -0500

    se agrego toda la paginacion

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -8,7 +8,7 @@ const Pagination = ({totalCount, onSelectPage, selectPage}) => {
     const pages = Math.ceil(totalCount/12); 
     return (
         <div className="flex justify-center mt-8 mb-8">
-            {[...Array(pages-6).keys()].map((page) => (
+            {[...Array(pages-1).keys()].map((page) => (
            <button onClick={ () => onSelectPage(page+1)} className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l ${parseInt(selectPage) === page + 1 ? "opacity-50 cursor-not-allowed" : ""}`}>
                 {page+1}
             </button>

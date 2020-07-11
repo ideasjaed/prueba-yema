49c7e2bc5794f59628e428be9e038b970777526e
unknown <ideas.jaed@gmail.com>
2020-07-10 23:03:16 -0500

    Updates

---
+++ precache-manifest.7fa57226d4c44ea5fda9b7a5c9e28741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bde57cda3971ab3196f108d267391a",
+    "url": "/prueba-yema/index.html"
+  },
+  {
+    "revision": "5eec1e3fb5255ad51c5a",
+    "url": "/prueba-yema/static/css/main.481239bd.chunk.css"
+  },
+  {
+    "revision": "bd77c17cc831a649b753",
+    "url": "/prueba-yema/static/js/2.a8d7b3e4.chunk.js"
+  },
+  {
+    "revision": "9d159c3d2ae614bb4135f8f59c25ed14",
+    "url": "/prueba-yema/static/js/2.a8d7b3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eec1e3fb5255ad51c5a",
+    "url": "/prueba-yema/static/js/main.7874e098.chunk.js"
+  },
+  {
+    "revision": "c29de45862f751c6110b",
+    "url": "/prueba-yema/static/js/runtime-main.194b2d95.js"
+  },
+  {
+    "revision": "c4bbeade4526847403be2fbf84fe87ce",
+    "url": "/prueba-yema/static/media/company.c4bbeade.jpg"
+  }
+]);
